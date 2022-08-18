e9bd7711a251a70ad2752e9ee24e71fd43697bf8
Spencer Miller <wspencer.miller@gmail.com>
2022-08-17 21:09:58 -0500

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const cors = require("cors");
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(5000, () => {
+    console.log("server has started on port");
+});
